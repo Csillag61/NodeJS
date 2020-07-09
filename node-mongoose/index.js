bef393a2af7c0d779c1415cc84ca1a5b6b83103c
Csillag61 <csillagok@gmail.com>
2020-07-09 10:24:10 -0400

    mongoose finished

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,21 +8,35 @@ const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log("Connected");
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test'
-    });
+    })
 
-    newDish.save()
+    
     .then((dish)=>{
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: {description: 'Updated test'}
+        }, 
+            {new: true
+        }).exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
 
-        return Dishes.deleteOne({});
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
     })
     .then (()=> {
         return mongoose.connection.close();
