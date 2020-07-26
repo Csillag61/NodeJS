f56913f3641a288c34d9d0159dfda775f5159dae
Csillag61 <csillagok@gmail.com>
2020-07-25 21:12:09 -0400

    file upload

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 
 const mongoose = require('mongoose');
@@ -62,6 +63,7 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 
 module.exports = app;
