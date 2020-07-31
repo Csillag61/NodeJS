24cfad7f31f52918bf81a2aa5d3b839e69864544
Csillag61 <csillagok@gmail.com>
2020-07-31 14:19:44 -0400

    face login

--- confusionServer/config.js
+++ confusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion', 
+    'facebook': {
+        clientId: '301324810982036',
+        clientSecret: '0619c3dfa516ebde476e27e9e54ccce0'
+    }
 }
